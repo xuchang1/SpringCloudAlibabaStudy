ec5eadb91c6cb3264c1a896680a760bb10b2eb88
1263919792@qq.com <1w2b3c4g>
2021-04-15 01:16:32 +0800

    spring-cloud-gateway-nacos

---
+++ spring-cloud-gateway-nacos/spring-cloud-gateway-nacos-consumer/src/main/java/com/study/ConsumerMain.java
@@ -0,0 +1,12 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMain.class, args);
+    }
+}
